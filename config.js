9686debb23fe31eae2226d35becc44458e68a1b5
jnutterdev <hello@jnutterdev.com>
2021-06-29 16:34:03 -0400

    Started new gatsby site based off of lumen starter project. Updated colors. Need to update blog posts and look at contentful

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by John Nutter',
+  subtitle: 'Full-stack developer and software engineer',
+  copyright: '©2021 All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'John Nutter',
+    photo: '/johnhead.jpg',
+    bio: 'Full-stack developer and software engineer. Passion for chiptunes and 80s synths',
+    contacts: {
+      email: 'hello@jnutterdev.com',
+      facebook: '',
+      telegram: '',
+      twitter: 'jnutterdev',
+      github: 'jnutterdev',
+      rss: '',
+      vkontakte: '',
+      linkedin: '',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
+};
